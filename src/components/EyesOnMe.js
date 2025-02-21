45100a2479e235dd6179460986ab9dfa2f7dc900
Bruno Trova <bltrova@gmail.com>
2025-02-21 06:49:39 -0800

    event-handling-lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
-// Code EyesOnMe Component Here
+import React from "react"
+
+
+
+
+function EyesOnMe() {
+
+    const handleEyes = () => {
+        console.log('Good!')
+    };
+
+    const handleBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    return (
+        <>
+            <button onFocus={handleEyes} onBlur={handleBlur}>Eyes on me</button>
+            
+        </>
+        
+        
+
+    )
+}
+
+
+export default EyesOnMe
